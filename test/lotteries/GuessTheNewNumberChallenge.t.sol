2a75192f7e30ea119598533cbb719e6cb6b3e1ba
twodam <twodam@protonmail.com>
2024-11-21 14:53:05 +0800

    feat(challenge): guess the new number

---
+++ test/lotteries/GuessTheNewNumberChallenge.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {GuessTheNewNumberChallenge} from "../../src/lotteries/GuessTheNewNumberChallenge.sol";
+
+contract GuessTheNewNumberChallengeTest is Test {
+    GuessTheNewNumberChallenge challenge;
+    receive() external payable {}
+
+    function setUp() public {
+        vm.deal(address(this), 1 ether);
+        challenge = new GuessTheNewNumberChallenge{value: 1 ether}();
+    }
+
+    function testDirectCalculation() public {
+        // this call is from a Contract
+        //   which share same block infos.
+        //   so we can just calculate the answer to "guess".
+        // we can deploy a proxy contract to do this 
+        //   if not within foundry test.
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
+        vm.deal(address(this), 1 ether);
+        challenge.guess{value: 1 ether}(answer);
+        
+        assertTrue(challenge.isComplete(), "not completed");
+    }
+}
